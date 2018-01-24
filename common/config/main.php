4380fc5e2037d30c5dce60e591291eea77f2a126
bytecrow <bytecrow@users.noreply.github.com>
2018-01-24 19:20:09 +0300

    enable pretty urls

--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,16 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'showScriptName' => false,
+            'enablePrettyUrl' => true,
+            'rules' => [
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ],
+        ],
     ],
 ];
