3e2ba3f5f6fe85b62a21c23c3ae18a510350c8a1
bytecrow <bytecrow@users.noreply.github.com>
2018-03-19 16:42:11 +0300

    Labels creating view

--- frontend/views/labels/new.php
+++ frontend/views/labels/new.php
@@ -6,30 +6,59 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 $this->title = Yii::t('app', 'Create New Label Group');
 
+$this->registerCss("
+#labels-tree .label { margin: 10px 0 0 20px; }
+#labels-tree .label-0 > .input-group { display: none; }
+");
+
 $this->registerJs("
+$('.js-add-label').on('click', function(e) {
+  const parent = $(this).closest('.label');
+  const label  = parent.clone(true);
+  const level  = $(parent).data('level') + 1;
 
-$('#m_repeater').repeater({            
-    initEmpty: false,
-   
-    defaultValues: {
-        'text-input': 'foo'
-    },
-     
-    show: function() {
-        $(this).slideDown();                               
-    },
+  $(label).removeClass('label-0');
+  $(label).children('.input-group').children('input').val('');
+  $(label).children('.child-labels').html('');
+  $(label).attr('data-level', level);
 
-    hide: function(deleteElement) {                 
-        if(confirm('Are you sure you want to delete this element?')) {
-            $(this).slideUp(deleteElement);
-        }                                
-    }      
+  $(label).appendTo( $(parent).children('.child-labels') );
 });
 
+$('.js-add').on('click', function(e) {
+  $('#labels-tree').find('.label :first').find('.js-add-label :first').trigger('click');
+});
+
+$('.js-del-label').on('click', function(e) {
+  e.preventDefault();
+  $(this).closest('.label').remove();
+});
+
+$('.js-save').on('click', function(e) {
+  e.preventDefault();
+  const labelsTree = createLabelTreeRecursievly($('.label-0').children('.child-labels'));
+  $('#labelgroup-labels_tree').val(labelsTree.lenght < 1 ? '' : JSON.stringify(labelsTree));
+  alert(JSON.stringify(labelsTree));
+  //$(this).submit();
+});
+
+function createLabelTreeRecursievly(childLabels) {
+  let labels = [];
+  $(childLabels).children('.label').each(function() {
+    const text = $(this).children('.input-group').children('input').val();
+    if (text.trim()==='') return;
+    const childLabels = $(this).children('.child-labels');
+    labels.push({[text]: createLabelTreeRecursievly(childLabels)});
+  });
+  return labels;
+}
+
+$('.js-add')
+  .trigger('click')
+  .trigger('click');
 ", yii\web\View::POS_READY);
 
 ?>
-
 
 <div class="row">
   <div class="col-lg-8">
@@ -59,65 +88,56 @@ $('#m_repeater').repeater({
                            ->textarea(['class'=>'form-control m-input', 'rows'=>'3'])
                            ->label(false) ?>
                 </div>
-
-	            <div class="m-section__sub"><?=Yii::t('app', 'Create labels tree here') ?></div>
-                <div class="col-md-8">
-	                <div class="form-group m-form__group">
-	                  <?= $form->field($model, 'labels_tree')
-	                           ->textInput(['class'=>'form-control m-input'])  // hiddenInput
-	                           ->label(false) ?>
-	                </div>
-	            </div>
-
-
-				<div id="m_repeater">
-					<div class="form-group  m-form__group row">
-						<div data-repeater-list="" class="col-lg-10">
-							<div data-repeater-item="" class="m--margin-bottom-10">
-								<div class="input-group">
-									<div class="input-group-prepend">
-										<button class="btn btn-warning" type="button">
-											<span>
-												<i class="la la-plus"></i>
-												<span>
-													Child
-												</span>
-											</span>
-										</button>
-									</div>
-									<input type="text" class="form-control form-control-danger" placeholder="Enter Label Text">
-									<div class="input-group-append">
-										<a href="#" class="btn btn-danger m-btn m-btn--icon">
-											<i class="la la-close"></i>
-										</a>
-									</div>
-								</div>
-							</div>
-
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-lg-1"></div>
-						<div class="col">
-							<div data-repeater-create="" class="btn btn btn-warning m-btn m-btn--icon">
-								<span>
-									<i class="la la-plus"></i>
-									<span>
-										Add
-									</span>
-								</span>
-							</div>
-						</div>
-					</div>
-				</div>
-
-
-
+  	            <div class="m-section__sub"><?=Yii::t('app', 'Create labels tree here') ?></div>
+                  <div class="col-md-8">
+  	                <div class="form-group m-form__group">
+  	                  <?= $form->field($model, 'labels_tree')
+  	                           ->hiddenInput(['class'=>'form-control m-input'])
+  	                           ->label(false) ?>
+  	                </div>
+  	            </div>
+          			<div id="labels-tree"  class="form-group  m-form__group row">
+        					<div class="col-lg-10">
+                    <div data-level="0" class="label label-0">
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <button class="btn btn-warning js-add-label" type="button">
+                            <span>
+                              <i class="la la-plus"></i>
+                              <span>
+                                <?=Yii::t('app', 'Child') ?>
+                              </span>
+                            </span>
+                          </button>
+                        </div>
+                        <input type="text" class="form-control form-control-danger" placeholder="Enter Label Text">
+                        <div class="input-group-append">
+                          <a href="#" class="btn btn-danger m-btn m-btn--icon js-del-label">
+                            <i class="la la-close"></i>
+                          </a>
+                        </div>
+                      </div>
+                      <div class="child-labels"></div>
+                    </div>
+        					</div>
+                </div>
+          			<div class="row">
+          				<div class="col-lg-1"></div>
+          				<div class="col">
+          					<div class="btn btn btn-warning m-btn m-btn--icon js-add">
+          						<span>
+          							<i class="la la-plus"></i>
+          							<span>
+          								<?=Yii::t('app', 'Add') ?>
+          							</span>
+          						</span>
+          					</div>
+          				</div>
+          			</div>
                 <div class="form-group m-form__group m--padding-top-20">
-                  <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-info m-btn--pill m-btn--air pull-right']) ?>
+                  <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-info m-btn--pill m-btn--air pull-right js-save']) ?>
                   <div class="clearfix"></div>
                 </div>
-              </form>
               <?php ActiveForm::end() ?>
               <div class="m--padding-bottom-20"></div>
             </div>
