373e6333c7f9839169f048b539eaf88071f05ead
bytecrow <bytecrow@users.noreply.github.com>
2018-03-23 16:23:36 +0300

    Blockchain callback model & controller created

---
+++ common/models/BlockchainCallback.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "blockchain_callback".
+ *
+ * @property int $id
+ * @property string $callback_id
+ * @property int $received
+ * @property int $success
+ * @property string $error
+ * @property string $payload
+ * @property int $created_at
+ * @property int $updated_at
+ */
+class BlockchainCallback extends \yii\db\ActiveRecord
+{
+    /**
+     * Callback Types
+     */
+    const DEPLOY_CONTRACT       = 1;    
+    const UPDATE_COMPLETED_WORK = 2;    
+    const FORCE_FINALIZE        = 3;    
+    const CREDIT_ACCOUNT        = 4;    
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'blockchain_callback';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['callback_id'], 'string', 'max' => 32],
+            [['callback_id'], 'required'],
+            [['callback_id'], 'unique'],
+            [['type'], 'integer'],
+            [['error', 'payload'], 'string'],
+            [['received', 'success'], 'boolean'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            \yii\behaviors\TimestampBehavior::className(),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'callback_id' => Yii::t('app', 'Callback ID'),
+            'received' => Yii::t('app', 'Received'),
+            'success' => Yii::t('app', 'Success'),
+            'error' => Yii::t('app', 'Error'),
+            'payload' => Yii::t('app', 'Payload'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+        ];
+    }
+
+}
---
+++ frontend/controllers/BlockchainCallbackController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace frontend\controllers;
+
+use common\models\Task;
+use common\models\BlockchainCallback;
+use yii\web\Controller;
+use yii\filters\AccessControl;
+use Yii;
+
+/**
+ * BlockchainCallbackController
+ * 
+ * Action names for callback must be the same as Gateway API methods
+ * names that returns callbacks
+ */
+class BlockchainCallbackController extends \yii\web\Controller
+{
+    public $enableCsrfValidation = false;
+
+    /**
+     * Array of callback request data 
+     */
+    protected $data;
+    /**
+     * Array of Callback-object saved params 
+     */
+    protected $params;
+
+	/**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@', '?'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+
+    public function beforeAction($action)
+    {
+        $this->data = Yii::$app->request->post();
+
+        $callback = BlockchainCallback::find()
+            ->where(['callback_id' => $this->data['taskId']])
+            ->one();
+
+        if ($callback === null) {
+            throw new \Exception("Invalid Callback id");
+        }
+
+        $callback->received = 1;
+        $callback->success = $this->data['success'] ?: '0';
+        $callback->error = json_encode($this->data['error']) ?: '';
+        $callback->payload = json_encode($this->data['payload']) ?: '';
+
+        if (!$callback->save()) {
+            throw new \Exception("Invalid Callback data");
+        }
+
+        $this->params = json_decode($callback->params, true);
+
+        return parent::beforeAction($action);
+    }
+
+    // We don't handle requests to index
+    //  public function actionIndex() {}
+
+    public function actionDeployContract()
+    {
+        $task = Task::findOne($this->params['task_id']);
+
+        if ($task === null) {
+            throw new \Exception("Can't find Task");
+        }
+
+        if (!$this->data['success']) {
+            // TODO: Handle errors
+
+            $task->status = Task::STATUS_CONTRACT_DEPLOYMENT_ERROR;
+            if (!$task->save()) {
+                throw new \Exception("Can't save Task");
+            }
+
+            Yii::$app->end();
+        } 
+
+        $task->contract_address = $this->data['payload']['contractAddress'];
+        $task->status = Task::STATUS_CONTRACT_NEW;
+
+        if (!$task->save()) {
+            throw new \Exception("Can't save Task");
+        }
+
+        Yii::$app->end();
+    }
+
+    public function actionUpdateCompletedWork()
+    {
+
+        Yii::$app->end();
+    }
+
+    public function actionForceFinalize()
+    {
+
+        Yii::$app->end();
+    }
+
+    public function actionCreditAccount()
+    {
+        // Here we do nothing for now.
+        Yii::$app->end();
+    }
+
+
+}
