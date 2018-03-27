058a6f5d32772e59cb70863f9e1feae07e862878
Juriy Panasevich <u.panasevich@graphgrail.com>
2018-03-27 15:04:50 +0300

    Assets. Moving components assets to resources directory

---
+++ frontend/resources/main/webfont-init.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    WebFont.load({
+        google: {"families":["Poppins:300,400,500,600,700","Roboto:300,400,500,600,700"]},
+        active: function() {
+            sessionStorage.fonts = true;
+        }
+    });
+});
