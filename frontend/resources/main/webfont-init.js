772edcf16bf1fd1f114af0c292999ff982ba0382
Juriy Panasevich <u.panasevich@graphgrail.com>
2018-03-28 10:56:53 +0300

    Assets. Change font to Roboto

--- frontend/resources/main/webfont-init.js
+++ frontend/resources/main/webfont-init.js
@@ -1,8 +1,6 @@
-$(document).ready(function () {
-    WebFont.load({
-        google: {"families":["Poppins:300,400,500,600,700","Roboto:300,400,500,600,700"]},
-        active: function() {
-            sessionStorage.fonts = true;
-        }
-    });
+WebFont.load({
+    google: {"families":["Roboto:300,400,500,600,700"]},
+    active: function() {
+        sessionStorage.fonts = true;
+    }
 });
