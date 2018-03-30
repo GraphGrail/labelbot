19e01d01da4baea21edad7982fda7a8b5b9540c7
Juriy Panasevich <elfelrandcor@gmail.com>
2018-03-30 15:22:28 +0300

    Cron command for pushing UpdateCompletedWorkJob to queue

--- console/controllers/BlockchainController.php
+++ console/controllers/BlockchainController.php
@@ -2,7 +2,10 @@
 
 namespace console\controllers;
 
+use common\models\Task;
+use console\jobs\UpdateCompletedWorkJob;
 use yii\console\ExitCode;
+use yii\log\Logger;
 
 class BlockchainController extends \yii\console\Controller
 {
@@ -10,10 +13,29 @@ class BlockchainController extends \yii\console\Controller
     public function actionUpdateCompletedWork()
     {
         try {
-            echo 'OK';
+            /** @var Task[] $tasks */
+            $tasks = Task::find()
+                ->active()
+                ->undeleted()
+                ->all();
+
+            foreach ($tasks as $task) {
+                try {
+                    $this->addToQueue($task);
+                } catch (\Exception $e) {
+                    \Yii::getLogger()->log($e->getMessage(), Logger::LEVEL_ERROR);
+                }
+            }
         } catch (\Exception $e) {
             echo $e->getMessage();
         }
         return ExitCode::OK;
+    }
+
+    protected function addToQueue(Task $task)
+    {
+        \Yii::$app->queue->push(new UpdateCompletedWorkJob([
+            'taskId' => $task->id,
+        ]));
     }
 }
