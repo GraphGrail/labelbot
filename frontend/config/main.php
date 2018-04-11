d8b3c8b055aea225ebfcaad1ca1c08df5824ef42
bytecrow <sovgit@gmail.com>
2018-04-11 19:04:52 +0300

    add smtp emails config

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-frontend',
+    'name'=>'GraphGrail Ai - LabelApp',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
